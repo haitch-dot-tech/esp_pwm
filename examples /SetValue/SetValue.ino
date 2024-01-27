61c226829cb1e800102cd557cbc8e2b79d43a7ad
haitch <henry.hall.au@gmail.com>
2024-01-27 18:16:15 +1100

    Update SetValue.ino

--- examples /SetValue/SetValue.ino
+++ examples /SetValue/SetValue.ino
@@ -2,7 +2,9 @@
   SetValue.ino
 
   A simple test of the core functionality of the esp_pwm library.
-  When used coorrectly, this code will set pin 25 on an ESP32 to 50% duty cycle at 5kHz.
+  When used correctly, this code will set an ESP32's pin 25 as a PWM output 
+  with internal channel 0, a 10 bit resolution and 5kHz frequency.
+  It then sets the pin to 50% duty cycle 
 */
 
 #include <esp_pwm.h>
@@ -10,9 +12,9 @@
 esp_pwm pwm; // Create a "pwm" object
 
 void setup() {
-  pwm.init(0, 25, 10, 5000); // Channel 0, Pin 25, 10 Bit Resolution, 5kHz Frequency
+    pwm.init(0, 25, 10, 5000); // Channel 0, Pin 25, 10 Bit Resolution, 5kHz Frequency
 }
 
 void loop() {
-  pwm.setOutput(50); // 50% Duty Cycle
+    pwm.setOutput(50); // 50% Duty Cycle
 }
